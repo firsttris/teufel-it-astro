34d1005102e9fd7d59e184425a25f70bfc6c4e83
Tristan Teufel <tristanteufel@googlemail.com>
2023-11-18 15:09:59 +0100

    migrate style to tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://firsttris.github.io',
-    base: '/teufel-it-astro',
+  site: 'https://firsttris.github.io',
+  base: '/teufel-it-astro',
+  integrations: [tailwind()]
 });
