4647f3d9e5aa0f4bcb8d0ccaa1a4415447a5bb17
Tristan Teufel <tristanteufel@googlemail.com>
2023-11-19 17:29:15 +0100

    update page

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,7 @@
 import { ui, defaultLang } from './ui';
 
-export const getLangFromUrl = (url: URL) => {
-  const lang = url.pathname.includes('/de') ? 'de' : 'en';
+export const getLangFromUrl = (pathname: string) => {
+  const lang = pathname.includes('/de') ? 'de' : 'en';
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
