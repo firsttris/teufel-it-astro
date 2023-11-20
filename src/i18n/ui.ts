eda295b0f2b6b5e627c6a540a6524ff557331ea3
Tristan Teufel <tristanteufel@googlemail.com>
2023-11-20 12:55:26 +0100

    add missing loca key

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,10 +10,12 @@ export const ui = {
         'business-title': 'Senior Software Developer',
         'academic-title': 'Business Informatics (B.Sc.)',
         'cv': 'Curriculum Vitae',
+        "source-code": "Source Code",
     },
     de: {
         'business-title': 'Senior Software Entwickler',
         'academic-title': 'Wirtschaftsinformatik (B.Sc.)',
         'cv': 'Lebenslauf',
+        "source-code": "Quellcode",
     },
 } as const;
