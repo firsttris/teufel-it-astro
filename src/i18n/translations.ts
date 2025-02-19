70e207fae4004894a842d6fa8cb9c6c1f69bd541
Tristan Teufel <tristanteufel@googlemail.com>
2025-02-19 07:28:32 +0100

    update translations

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -6,13 +6,27 @@ export const ui = {
         'academic-title': 'Business Informatics (B.Sc.)',
         'cv': 'Curriculum Vitae',
         "source-code": "Source Code",
-        "generated": "Generated at:"
+        "generated": "Generated at:",
+        "subtitle": "I am an expert in Clean-Code, Test Automation and Test-Driven Development (TDD).",
+        "current": "Currently I work as a freelance software developer and have worked on many exciting {{projects}} in the past 10 years.",
+        "projects": "projects",
+        "in-my-freetime": "In my free time I work on open-source projects on {{github}}.",
+        "best-project": "One of my most successful projects is {{jestRunner}} a VSCode extension for test execution, which has been installed as a recommended extension by {{nx}} since 2020.",
+        "contactMe": "If you need support in developing software, you can contact me {{contact}}.",
+        "contact": "Contact me"
     },
     de: {
         'business-title': 'Senior Software Entwickler',
         'academic-title': 'Wirtschaftsinformatik (B.Sc.)',
         'cv': 'Lebenslauf',
         "source-code": "Quellcode",
-        "generated": "Generiert am:"
+        "generated": "Generiert am:",
+        "subtitle": "Ich bin Experte für Clean-Code, Testautomatisierung und Testgetriebene Entwicklung (TDD).",
+        "current": "Derzeit arbeite ich als selbständiger Softwareentwickler und habe in den vergangenen 10 Jahren viele spannende {{projects}} realisiert.",
+        "projects": "Projekte",
+        "in-my-freetime": "In meiner Freizeit arbeite ich an Open-Source Projekten auf {{github}}.",
+        "best-project": "Eines meiner erfolgreichstes Projekt ist {{jestRunner}} eine VSCode-Erweiterung zur Testausführung, die seit 2020 als empfohlene Erweiterung von {{nx}} installiert wird.",
+         "contactMe": "Falls Sie Unterstützung bei der Entwicklung von Software benötigen, können Sie mich gerne {{contact}}.",
+         "contact": "Kontaktieren"
     },
 } as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,3 +1,4 @@
+import type { JSX } from 'astro/jsx-runtime';
 import { ui, defaultLang } from './translations';
 
 const getLangFromUrl = (pathname: string) => {
@@ -7,8 +8,26 @@ const getLangFromUrl = (pathname: string) => {
 }
 
 export const useTranslations = (pathname: string) => {
-    const lang = getLangFromUrl(pathname);
-    return (key: keyof typeof ui[typeof defaultLang]) => {
-      return ui[lang][key] || ui[defaultLang][key];
+  const lang = getLangFromUrl(pathname);
+  return (
+    key: keyof typeof ui[typeof defaultLang],
+    params?: Record<string, JSX.Element>
+  ): (string | JSX.Element)[] | string => {
+    const translation = ui[lang][key] || ui[defaultLang][key];
+
+    if (!params) {
+      return translation;
     }
+
+    // Teile den String an den Platzhaltern, die mit {{ platsholder }} definiert sind.
+    const parts = translation
+      .split(/({{\s*\w+\s*}})/g)
+      .filter(Boolean)
+      .map(part => {
+        const match = part.match(/{{\s*(\w+)\s*}}/);
+        return match ? params[match[1]] : part;
+      });
+
+    return parts;
   }
+}
