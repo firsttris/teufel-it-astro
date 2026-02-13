a8abe263fb132c422d936b76a07c8e6745e20526
Tristan Teufel <tristanteufel@googlemail.com>
2026-02-13 10:11:23 +0100

    update astro & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
-
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teufel-it.de/',
-  integrations: [tailwind(), icon({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [icon({
     include: {
-      mdi: ['email', 'menu', 'close', 'arrow-right', 'file-account' ],
+      mdi: ['email', 'menu', 'close', 'arrow-right', 'file-account'],
       'simple-icons': ['github', 'linkedin'],
-
     },
-  }), mdx()]
+  })]
 });
